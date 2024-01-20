9876834fe7bf1ba451e7fbf67533615099b52160
Pankaj Atri <pankajatri91@gmail.com>
2024-01-21 01:44:18 +0530

    connect to DataBase

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
-import express from 'express' // as we made type ->module we can use import insted of require
+import express from 'express'; // as we made type ->module we can use import insted of require
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); //to use env file first instal and config dotenv
 const app=express();
 
+//connect to database
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+
+  //connect to server
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
   });
+
