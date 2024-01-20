39b497f5c80d17e851d54029beac92a796331ac2
Pankaj Atri <pankajatri91@gmail.com>
2024-01-20 16:14:39 +0530

    instal react+vite and tailwind css and setup first react app

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/*give path in content:[] to scan all this file for giving Tailwind css template*/
