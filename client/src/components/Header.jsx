62b73d6e03b9a6e26d6fd38babf4686cee928425
Pankaj Atri <pankajatri91@gmail.com>
2024-01-25 00:26:37 +0530

    Add functionality to signup page,Post Request,Route change to server using Proxy in vite.config.js, useState,useNavigate,store data in db..,errorhandling,Loding feature..

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -41,7 +41,7 @@ export default function Header() {
           <FaMoon /> {/* moon icon */}
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+          <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
         </Link>
         <Navbar.Toggle />   {/* hamberger for toggling during screen size change */}
        
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+  proxy: {
+    '/api': {  //on fetch  we change path to server port of route wher we found /api in path we add target"..3000"to that
+      target: 'http://localhost:3000',
+      secure: false,
+    },
+  },
+},
   plugins: [react()],
 })
