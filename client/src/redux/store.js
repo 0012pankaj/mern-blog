948d5f4c09030b7db31b7c1ed28e7cd56400c860
Pankaj Atri <pankajatri91@gmail.com>
2024-01-27 18:30:33 +0530

    Add redux-persist to store the state in a local storage..

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+ //use redux-persist for  store the state in a local storage so that on refresh page we dont loose our state
+ //create root reducer for 1 for all reducer
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+           //1.persist reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+ middleware: (getDefaultMiddleware) =>
+   getDefaultMiddleware({ serializableCheck: false }), //middel ware to handel any error
 });
+                        //2 persist store
+export const persistor = persistStore(store);
