d61e5d0a16ec4bc8d4102b4fe98ac4e1ae9f8523
Pankaj Atri <pankajatri91@gmail.com>
2024-01-28 19:17:18 +0530

    Update Header Component with User Data (user profile and Dropdown)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,15 @@
 
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
-    const path = useLocation().pathname;  //assign current page path in path variable hel in sowing "active" nav icon 
-  return (
+    const path = useLocation().pathname;  //assign current page path in path variable help in sowing "active" nav icon 
+      const { currentUser } = useSelector((state) => state.user); //fetch curent user detail from redux store state to show info and profile
+
+    return (
     <Navbar className='border-b-2'>
 
       {/* header icon with link tag of react router dom to move to req page*/}
@@ -40,22 +43,46 @@ export default function Header() {
         <Button className='w-12 h-10 ' color='gray' pill>
           <FaMoon /> {/* moon icon */}
         </Button>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
-        </Link>
+        {/* if user login than show detail an profile pic else show Sign in button */}
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToPink' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
         <Navbar.Toggle />   {/* hamberger for toggling during screen size change */}
        
       </div>
 
       {/*  <Navbar.Toggle /> &   <Navbar.Collapse> work together acc to Screen size */}
       <Navbar.Collapse>
-        <Navbar.Link href="/"  active={path === "/"}>{/* active={path === "/"} if path match to given page show it coloured->active */}
+        <Navbar.Link href='/'  active={path === '/'}>{/* active={path === "/"} if path match to given page show it coloured->active */}
           Home
         </Navbar.Link>
-        <Navbar.Link href="/about" active={path === "/about"}>
+        <Navbar.Link href='/about' active={path === '/about'}>
           About
         </Navbar.Link>
-        <Navbar.Link href="/projects" active={path === "/projects"}>
+        <Navbar.Link href='/projects' active={path === '/projects'}>
             Projects
         </Navbar.Link>
       </Navbar.Collapse>
