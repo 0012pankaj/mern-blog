897c08c57f2997b1b57dd513ce0013b94c94b2b6
Pankaj Atri <pankajatri91@gmail.com>
2024-01-30 01:05:04 +0530

    Create sidebar of the dashboard Page

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,37 @@
+import { Sidebar } from 'flowbite-react';
+import { HiUser, HiArrowSmRight } from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashSidebar() {
+  const location = useLocation(); //give current path like "http://localhost:5173/dashboard?tab=profile"
+  const [tab, setTab] = useState(''); //initially null
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search); //it extract path after ? like "?tab=profile"
+    const tabFromUrl = urlParams.get('tab'); //it give value of tap which is "profile"
+    if (tabFromUrl) {
+      setTab(tabFromUrl); 
+    }
+  }, [location.search]); //every time path change it rerender and asign or set value to tab
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={'User'}
+              labelColor='dark'
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
+  const location = useLocation(); //give current path like "http://localhost:5173/dashboard?tab=profile"
+  const [tab, setTab] = useState(''); //initially null
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search); //it extract path after ? like "?tab=profile"
+    const tabFromUrl = urlParams.get('tab'); //it give value of tap which is "profile"
+    if (tabFromUrl) {
+      setTab(tabFromUrl); 
+    }
+  }, [location.search]); //every time path change it rerender and asign or set value to tab
   return (
-    <div>
-      DashBoard
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
     </div>
-  )
+  );
 }
