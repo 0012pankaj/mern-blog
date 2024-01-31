20722535aa63cdebbb577ef3b0c042b5ea857607
Pankaj Atri <pankajatri91@gmail.com>
2024-01-31 18:02:40 +0530

    Add Profile image file uploding functionality (convert file to url)

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,16 +1,46 @@
 import { Button, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 
 export default function DashProfile() {
     const { currentUser } = useSelector((state) => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const filePickerRef = useRef();
+   
+    const handleImageChange = (e) => {
+      const file = e.target.files[0]; //give use file object contain file detail not URL
+      if (file) {
+        setImageFile(file); 
+        setImageFileUrl(URL.createObjectURL(file)); //convert file to Urland Seturl
+      }
+    };
+    // console.log(imageFile,imageFileUrl)
+    useEffect(() => {
+      if (imageFile) {
+        uploadImage();
+      }
+    }, [imageFile]);
+  
+    const uploadImage =  () => {
+       console.log("uploding Image.apply.....");
+    }
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
     <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
     <form className='flex flex-col gap-4'>
-      <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-        <img
-          src={currentUser.profilePicture}
+       <input
+          type='file'
+          accept='image/*'
+          onChange={handleImageChange}
+          hidden
+          ref={filePickerRef}
+         
+        />
+      <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={() => filePickerRef.current.click()}>
+        <img 
+          src={imageFileUrl || currentUser.profilePicture}
           alt='user'
           className='rounded-full w-full h-full object-cover border-8 border-[#9381f64e]'
         />
