e5290f2b8000f28bd472a5371a44a7db286cac1c
Pankaj Atri <pankajatri91@gmail.com>
2024-01-31 18:58:56 +0530

    Complete uploadImage Functionality add Firebase Storage to store image

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,6 +1,14 @@
-import { Button, TextInput } from 'flowbite-react';
+import { Alert, Button, TextInput } from 'flowbite-react';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
 
 
 export default function DashProfile() {
@@ -8,7 +16,9 @@ export default function DashProfile() {
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const filePickerRef = useRef();
-   
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
+   console.log(imageFileUploadProgress,imageFileUploadError);
     const handleImageChange = (e) => {
       const file = e.target.files[0]; //give use file object contain file detail not URL
       if (file) {
@@ -24,8 +34,32 @@ export default function DashProfile() {
     }, [imageFile]);
   
     const uploadImage =  () => {
-       console.log("uploding Image.apply.....");
-    }
+      //  console.log("uploding Image.apply.....");
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + imageFile.name; //for uniquename during storage
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  
+          setImageFileUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageFileUploadError(
+            'Could not upload image (File must be less than 2MB)'
+          );
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageFileUrl(downloadURL);
+          });
+        }
+      );
+    };
+  
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
     <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -45,6 +79,9 @@ export default function DashProfile() {
           className='rounded-full w-full h-full object-cover border-8 border-[#9381f64e]'
         />
       </div>
+      {imageFileUploadError && (
+          <Alert color='failure'>{imageFileUploadError}</Alert>
+        )}
       <TextInput
         type='text'
         id='username'
@@ -69,3 +106,21 @@ export default function DashProfile() {
   </div>
 );
 }
+
+
+
+// rules_version = '2'; ------------Rule Set in Firebase acess
+
+// // Craft rules based on data in your Firestore database
+// // allow write: if firestore.get(
+// //    /databases/(default)/documents/users/$(request.auth.uid)).data.isAdmin;
+// service firebase.storage {
+//   match /b/{bucket}/o {
+//     match /{allPaths=**} {
+//            allow read;
+//            allow write: if
+//          request.resource.size < 2 * 1024 * 1024 &&
+//           request.resource.contentType.matches('image/.*')
+//     }
+//   }
+// }
