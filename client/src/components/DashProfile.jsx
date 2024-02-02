e788bbc9a93d704bc94d391f1e79d8ea435e7a8e
Pankaj Atri <pankajatri91@gmail.com>
2024-02-02 19:43:44 +0530

    Complete Delete user functionality ( Use Modal component of flowbite to show popup window)

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,7 +1,7 @@
-import { Alert, Button, TextInput } from 'flowbite-react';
+import { Alert,Modal, Button, TextInput } from 'flowbite-react';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import {
   getDownloadURL,
   getStorage,
@@ -16,11 +16,14 @@ import {
   updateStart,
   updateSuccess,
   updateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 export default function DashProfile() {
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser,error } = useSelector((state) => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const filePickerRef = useRef();
@@ -31,6 +34,8 @@ export default function DashProfile() {
     const [imageFileUploading, setImageFileUploading] = useState(false);
     const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
     const [updateUserError, setUpdateUserError] = useState(null);
+
+    const [showModal, setShowModal] = useState(false);
   
   //  console.log(imageFileUploadProgress,imageFileUploadError);
     const handleImageChange = (e) => {
@@ -81,7 +86,7 @@ export default function DashProfile() {
         }
       );
     };
-    //-----------------------------------work during submition updated data------------
+    //-----------------------------------Update user------------
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -121,6 +126,25 @@ export default function DashProfile() {
     }
   }
   
+  //delete user----------------------------------
+  const handleDeleteUser = async () => {
+    setShowModal(false);
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(deleteUserFailure(data.message));
+      } else {
+        dispatch(deleteUserSuccess(data));
+      }
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
     <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -186,7 +210,7 @@ export default function DashProfile() {
       </Button>
     </form>
     <div className="text-red-500 flex justify-between mt-5">
-      <span className='cursor-pointer'>Delete Account</span>
+      <span className='cursor-pointer' onClick={() => setShowModal(true)} >Delete Account</span>
       <span className='cursor-pointer'>Sign Out</span>
     </div>
     {updateUserSuccess && (
@@ -200,6 +224,39 @@ export default function DashProfile() {
           {updateUserError}
         </Alert>
       )}
+
+        {error && (
+        <Alert color='failure' className='mt-5'>
+          {error}
+        </Alert>
+      )}
+       
+       {/* mode shown during click on Delete Account , copy from Flow bite react  */}
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete your account?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+      
       
   </div>
 );
